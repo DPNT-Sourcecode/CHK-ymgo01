a41a9e299616215aee8e750d0cd15b336805e06b
sbx_user1051 <sbx_user1051@169.254.11.141>
2021-09-27 20:10:26 +0000

    Mon Sep 27 20:10:26 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,18 @@
 
+PRICE_TABLE = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15,
+}
+
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str):
-    
+    item_to_price_map = {}
+
+    for item in skus:
+        if 
+
 
