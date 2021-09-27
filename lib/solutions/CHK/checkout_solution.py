54fa1b276e42019c2678247d455f5c065bdb46c4
sbx_user1051 <sbx_user1051@169.254.11.141>
2021-09-27 20:13:26 +0000

    Mon Sep 27 20:13:26 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,13 +6,21 @@ PRICE_TABLE = {
     'D': 15,
 }
 
+OFFERS = {
+    '3A': 130,
+    '2B': 45
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str):
-    item_to_price_map = {}
+    item_count_map = {}
 
-    for item in skus:
-        if 
-
-
+    for s in skus:
+        if s not in PRICE_TABLE: # also handle case if there is no sku inputted
+            return -1
+        
+        if s not in item_count_map:
+            item_count_map[s] = 1
+        else:
+            item_count_map[s] += 1
