43e1900bf886f903cacbc12ed67825e724f22258
sbx_user1051 <sbx_user1051@169.254.37.45>
2021-09-28 15:25:38 +0000

    Tue Sep 28 15:25:38 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,28 +1,8 @@
+from collections import namedtuple
+from dataclasses import dataclass
 
-PRICE_TABLE = {
-    'A': 50,
-    'B': 30,
-    'C': 20,
-    'D': 15,
-}
-
-OFFERS = {
-    'A': 130,
-    'B': 45
-}
-
+INVALID_SKUS = -1
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str):
-    item_count_map = {}
-
-    for s in skus:
-        if s not in PRICE_TABLE: # also handle case if there is no sku inputted
-            return -1
-
-        if s not in item_count_map:
-            item_count_map[s] = 1
-        else:
-            item_count_map[s] += 1
-    
-    item_count
+    pass
