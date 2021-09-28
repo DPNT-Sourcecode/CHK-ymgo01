905fba020148462d62d6d7198f8b5fde8675cb2b
sbx_user1051 <sbx_user1051@169.254.37.45>
2021-09-28 15:55:37 +0000

    Tue Sep 28 15:55:37 UTC 2021

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -15,4 +15,11 @@ class TestCheckout():
         assert checkout_solution.checkout('ABCD') == 115
 
     def test_invalid_input_space(self):
-        assert checkout_solution.checkout('ABC D') == 115
+        assert checkout_solution.checkout('ABC D') == -1
+
+    def test_3A2B(self):
+        assert checkout_solution.checkout('ABABA') == 175
+
+    def test_3C2D(self):
+        assert checkout_solution.checkout('CCDCD') == 90
+
