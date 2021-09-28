461c9ff27d4ac39bc92ebc180ce5fa8539f3fe51
sbx_user1051 <sbx_user1051@169.254.37.45>
2021-09-28 15:34:37 +0000

    Tue Sep 28 15:34:37 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,14 +15,22 @@ class Item:
 
 SKU_TO_ITEM = {
     "A": Item("A", 50, deal(3, 130)),
-    "B": Item("B", 50, deal(3, 130)),
-    "C": Item("C", 50),
-    "D": Item("D", 50),
+    "B": Item("B", 30, deal(2, 45)),
+    "C": Item("C", 20),
+    "D": Item("D", 15),
 }
+
+
+def skus_valid(skus: str) -> bool:
+    '''Determine whether the input is valid
+    '''
+    return all([sku in SKU_TO_ITEM for sku in skus])
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str):
-    pass
+    if not skus_valid(skus):
+        return INVALID_SKUS
+
 
 
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,7 +3,8 @@ from solutions.CHK import checkout_solution
 
 class TestCheckout():
     def test_A(self):
-        assert checkout_solution.checkout('A') == 3
+        assert checkout_solution.checkout('A') == 50
 
     def test_3A(self):
-        assert checkout_solution.checkout('AAA') == 3
+        assert checkout_solution.checkout('AAA') == 130
+
