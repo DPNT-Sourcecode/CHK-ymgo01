687434ae32c7666db601a443182edf7cbf823312
sbx_user1051 <sbx_user1051@169.254.37.45>
2021-09-28 15:28:37 +0000

    Tue Sep 28 15:28:37 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,8 +1,16 @@
 from collections import namedtuple
 from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class Item:
+    sku: str
+    unit_price: int
+    special_deal: Optional[tuple]
 
 INVALID_SKUS = -1
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str):
     pass
+
