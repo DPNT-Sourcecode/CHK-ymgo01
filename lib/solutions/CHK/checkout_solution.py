14f54a636e0e7c8a1cbdaae0f6acb4eee3229c9c
sbx_user1051 <sbx_user1051@169.254.37.45>
2021-09-28 15:31:37 +0000

    Tue Sep 28 15:31:37 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,16 +1,28 @@
 from collections import namedtuple
 from dataclasses import dataclass
-from typing import Optional
+from typing import NamedTuple, Optional
+
+INVALID_SKUS = -1
+
+deal = namedtuple("SpecialOffer", ["quantity", "offer_price"])
 
 @dataclass
 class Item:
     sku: str
     unit_price: int
-    special_deal: Optional[tuple]
+    special_deal: Optional[NamedTuple]
 
-INVALID_SKUS = -1
+
+SKU_TO_ITEM = {
+    "A": Item("A", 50, deal(3, 130)),
+    "B": Item("B", 50, deal(3, 130)),
+    "C": Item("C", 50),
+    "D": Item("D", 50),
+}
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str):
     pass
+
 
